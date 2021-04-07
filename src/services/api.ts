4a8522157b2a16fb79e04413327d4bb6c72fdf3b
Hugo <c1297097@bb.com.br>
2021-04-07 11:13:58 -0300

    Url de producao

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.0.10:3333',
+    baseURL: 'https://rifatudo-backend.herokuapp.com',
 });
 
 export default api;
